21759a628a732536ebc259629d53460b10b7da34
CinisBorn <zafkielc@gmail.com>
2026-02-26 14:04:24 -0300

    feat: add interval by specific time measurements

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 use governor::{DefaultDirectRateLimiter, Quota, RateLimiter};
-use std::num::{NonZeroU32};
 use reqwest::Client;
+use std::num::NonZeroU32;
+
+pub enum TimeMeasurement {
+    BySeconds,
+    ByMinutes,
+    ByHours,
+}
 
 pub struct ClientBuilder {
     limit: DefaultDirectRateLimiter,
@@ -19,11 +25,17 @@ impl Host {
 }
 
 impl ClientBuilder {
-    pub fn build(quota: NonZeroU32) -> Host {
+    pub fn build(quota: NonZeroU32, interval: TimeMeasurement) -> Host {
         let burst = NonZeroU32::new(1).expect("No Zero Expected");
-        let quota = Quota::per_second(quota).allow_burst(burst);
-        let limit = RateLimiter::direct(quota);
         
-        Host { config: Self { client: Client::new(), limit}}
+        let quota_limit = match interval {
+            TimeMeasurement::ByHours => Quota::per_hour(quota).allow_burst(burst),
+            TimeMeasurement::ByMinutes => Quota::per_minute(quota).allow_burst(burst),
+            TimeMeasurement::BySeconds => Quota::per_second(quota).allow_burst(burst),
+        };
+        
+        let limit = RateLimiter::direct(quota_limit);
+        
+        Host { config: Self { client: Client::new(), limit }}
     }
 }
