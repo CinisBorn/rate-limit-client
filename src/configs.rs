1bd37ca5661db26b5f92c9e5dcfeed337c22777e
CinisBorn <zafkielc@gmail.com>
2026-03-17 14:53:28 -0300

    doc: add docs

--- src/configs.rs
+++ src/configs.rs
@@ -2,6 +2,20 @@ use std::{num::NonZeroU32, ops::Deref};
 use crate::{TimeInterval};
 use governor::clock::Clock;
 
+/// A config `struct` for build clients and hosts.
+/// ```rust
+/// use http_client::configs::Config;
+/// use http_client::{TimeInterval, RateLimitClient};
+/// use std::num::NonZeroU32;
+/// 
+/// fn main() {
+///     let client = RateLimitClient::build(Config {
+///         quota: NonZeroU32::new(10).unwrap(),
+///         burst: NonZeroU32::new(2).unwrap(),
+///         interval: TimeInterval::ByMinutes,
+///     });
+/// }
+/// ```
 pub struct Config {
     pub quota: NonZeroU32,
     pub burst: NonZeroU32,
@@ -14,7 +28,34 @@ impl Clone for Config {
         *self
     }
 }
-
+/// A configuration `struct` for build `hosts`. 
+/// ```rust
+/// use http_client::configs::{Config, HostConfig};
+/// use http_client::{TimeInterval, RateLimitClient};
+/// use std::num::NonZeroU32;
+/// 
+/// fn main() {
+///     let config = Config {
+///         quota: NonZeroU32::new(10).unwrap(),
+///         burst: NonZeroU32::new(2).unwrap(),
+///         interval: TimeInterval::ByMinutes,
+///     };
+///     let client = RateLimitClient::build(config);
+///     
+///     client.build_host(HostConfig {
+///         base: config,
+///         hostname: "google.com"
+///     });
+///     client.build_host(HostConfig {
+///         base: Config {
+///             quota: NonZeroU32::new(12).unwrap(),
+///             burst: NonZeroU32::new(1).unwrap(),
+///             interval: TimeInterval::ByMinutes,
+///         },
+///         hostname: "localhost.com"
+///     });
+/// }
+/// ```
 pub struct HostConfig {
     pub base: Config,
     pub hostname: &'static str,
@@ -34,7 +75,13 @@ impl Clone for HostConfig {
         *self
     }
 }
-
+/// Creates a configuration to clients for testing purposes. 
+/// ```ignore
+/// let config = RateLimitClient::build_with_clock(ConfigWithClock {
+///     base: configs,
+///     clock: FakeRelativeClock::default()
+/// });
+/// ```
 pub struct ConfigWithClock<C: Clock + Clone>{
     pub base: Config,
     pub clock: C,
